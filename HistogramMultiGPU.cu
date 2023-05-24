b14552b4dbaf0fe1fd62d4021618d07594794b74
OriolMarcoDeumal <113381406+OriolMarcoDeumal@users.noreply.github.com>
2023-05-24 11:37:44 +0200

    Update and rename HistogramMultiGPU to HistogramMultiGPU.cu

--- HistogramMultiGPU.cu
+++ HistogramMultiGPU.cu
@@ -1,3 +1,17 @@
+#include <iostream>
+#include <numeric>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/times.h>
+#include <sys/resource.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+using namespace std;
+
 // Function to check the number of GPUs
 void checkDeviceCount(int &numDevices) {
     cudaGetDeviceCount(&numDevices);
@@ -8,17 +22,92 @@ void checkDeviceCount(int &numDevices) {
 }
 
 // Kernel functions
-// ... [Rest of the code same as before] ...
+__global__ void histogram_kernel(unsigned char *input_ptr, int *histogram, int width, int height, int channels) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int px = idx * channels;
+    if (idx < width * height) {
+        int Y = input_ptr[px];
+        atomicAdd(&histogram[Y], 1);
+    }
+}
+
+__global__ void equalize_kernel(unsigned char *input_ptr, int *histogram_equalized, int width, int height, int channels) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int px = idx * channels;
+    if (idx < width * height) {
+        int value_before = input_ptr[px];
+        int value_after = histogram_equalized[value_before];
+        input_ptr[px] = value_after;
+    }
+}
+
+__global__ void ycbcr_kernel(unsigned char *input_ptr, int width, int height, int channels, bool toYCbCr) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int px = idx * channels;
+
+    if (idx < width * height) {
+        int r = input_ptr[px];
+        int g = input_ptr[px + 1];
+        int b = input_ptr[px + 2];
+    
+        if (toYCbCr) {
+            int Y = (int) (16 + 0.25679890625 * r + 0.50412890625 * g + 0.09790625 * b);
+            int Cb = (int) (128 - 0.168736 * r - 0.331264 * g + 0.5 * b);
+            int Cr = (int) (128 + 0.5 * r - 0.418688 * g - 0.081312 * b);
+
+            input_ptr[px + 0] = Y;
+            input_ptr[px + 1] = Cb;
+            input_ptr[px + 2] = Cr;
+        } else {
+            int Y = r;
+            int Cb = g;
+            int Cr = b;
+
+            int R = max(0, min(255, (int) (Y + 1.402 * (Cr - 128))));
+            int G = max(0, min(255, (int) (Y - 0.344136 * (Cb - 128) - 0.714136 * (Cr - 128))));
+            int B = max(0, min(255, (int) (Y + 1.772 * (Cb - 128))));
+
+            input_ptr[px + 0] = R;
+            input_ptr[px + 1] = G;
+            input_ptr[px + 2] = B;
+        }
+    }
+}
+
+// Function to check CUDA errors
+void CheckCudaError(char sms[], int line) {
+    cudaError_t error;
+    error = cudaGetLastError();
+    if (error != cudaSuccess) {
+        printf("CUDA error: %s at line %d\n", sms, line);
+        exit(-1);
+    }
+}
 
 int main(int argc, char** argv) {
     // Check the number of GPUs
     int numDevices;
     checkDeviceCount(numDevices);
 
-    // [Rest of the code same as before...]
+    if (argc != 3) {
+        cout << "Usage: " << argv[0] << " <input_file> <output_file>" << endl;
+        return (-1);
+    }
+
+    char* fileIN = argv[1];
+    char* fileOUT = argv[2];
 
     // Load image
-    // [Rest of the code same as before...]
+    int width, height, channels;
+    unsigned char* image = stbi_load(fileIN, &width, &height, &channels, 0);
+    if (!image) {
+        fprintf(stderr, "Couldn't load image.\n");
+        return (-1);
+    }
+    // Allocate memory on the GPU
+    unsigned char* d_image;
+    cudaMalloc((void **)&d_image, width * height * channels * sizeof(unsigned char));
+    cudaMemcpy(d_image, image, width * height * channels * sizeof(unsigned char), cudaMemcpyHostToDevice);
 
     // Define grid and block dimensions
     dim3 block_dim(256, 1, 1);
