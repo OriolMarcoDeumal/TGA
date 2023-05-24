11178b27201203b33044b2d65ba0d3f64bfd8d65
OriolMarcoDeumal <113381406+OriolMarcoDeumal@users.noreply.github.com>
2023-05-24 11:38:54 +0200

    Update and rename HistogramPinned to HistogramPinned.cu

--- HistogramPinned.cu
+++ HistogramPinned.cu
@@ -1,3 +1,79 @@
+#include <iostream>
+#include <numeric>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/times.h>
+#include <sys/resource.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+using namespace std;
+
+// Kernel functions
+__global__ void histogram_kernel(unsigned char *input_ptr, int *histogram, int width, int height, int channels) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int px = idx * channels;
+    if (idx < width * height) {
+        int Y = input_ptr[px];
+        atomicAdd(&histogram[Y], 1);
+    }
+}
+
+__global__ void equalize_kernel(unsigned char *input_ptr, int *histogram_equalized, int width, int height, int channels) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int px = idx * channels;
+    if (idx < width * height) {
+        int value_before = input_ptr[px];
+        int value_after = histogram_equalized[value_before];
+        input_ptr[px] = value_after;
+    }
+}
+
+__global__ void ycbcr_kernel(unsigned char *input_ptr, int width, int height, int channels, bool toYCbCr) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int px = idx * channels;
+
+    if (idx < width * height) {
+        int r = input_ptr[px];
+        int g = input_ptr[px + 1];
+        int b = input_ptr[px + 2];
+    
+        if (toYCbCr) {
+            int Y = (int) (16 + 0.25679890625 * r + 0.50412890625 * g + 0.09790625 * b);
+            int Cb = (int) (128 - 0.168736 * r - 0.331264 * g + 0.5 * b);
+            int Cr = (int) (128 + 0.5 * r - 0.418688 * g - 0.081312 * b);
+
+            input_ptr[px + 0] = Y;
+            input_ptr[px + 1] = Cb;
+            input_ptr[px + 2] = Cr;
+        } else {
+            int Y = r;
+            int Cb = g;
+            int Cr = b;
+
+            int R = max(0, min(255, (int) (Y + 1.402 * (Cr - 128))));
+            int G = max(0, min(255, (int) (Y - 0.344136 * (Cb - 128) - 0.714136 * (Cr - 128))));
+            int B = max(0, min(255, (int) (Y + 1.772 * (Cb - 128))));
+
+            input_ptr[px + 0] = R;
+            input_ptr[px + 1] = G;
+            input_ptr[px + 2] = B;
+        }
+    }
+}
+
+// Function to check CUDA errors
+void CheckCudaError(char sms[], int line) {
+    cudaError_t error;
+    error = cudaGetLastError();
+    if (error != cudaSuccess) {
+        printf("CUDA error: %s at line %d\n", sms, line);
+        exit(-1);
+    }
+}
 int main(int argc, char** argv) {
     if (argc != 3) {
         cout << "Usage: " << argv[0] << " <input_file> <output_file>" << endl;
